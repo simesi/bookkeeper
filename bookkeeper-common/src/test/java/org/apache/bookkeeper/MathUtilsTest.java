1b662431ac6e09b9e56540e49a095a2032b9e5bf
Simesi Ubuntu <simesi@hotmail.it>
2020-09-06 16:56:51 +0200

    Deleted some tests and added others

---
+++ bookkeeper-common/src/test/java/org/apache/bookkeeper/MathUtilsTest.java
@@ -0,0 +1,37 @@
+package org.apache.bookkeeper;
+import org.apache.bookkeeper.common.util.MathUtils;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class MathUtilsTest {
+
+	 // int signSafeMod(long dividend, int divisor)
+	
+	@Test (expected = java.lang.ArithmeticException.class)
+	public void signSafeModTest() {           //invalid input
+		Assert.assertTrue(MathUtils.signSafeMod(0, 0)==0);
+		
+	}
+	@Test 
+	public void signSafeModTest2() {           //valid input
+		Assert.assertTrue(MathUtils.signSafeMod(3, 2)==1);
+		
+	}
+	
+	//int findNextPositivePowerOfTwo(final int value)-----------
+	
+	@Test
+	public void findNextPositivePowerOfTwoTest() {          //valid input
+		     Assert.assertTrue(MathUtils.findNextPositivePowerOfTwo(9)==16);
+	}
+	@Test
+	public void findNextPositivePowerOfTwoTest1() {          
+		     Assert.assertTrue(MathUtils.findNextPositivePowerOfTwo(0)==1);
+	}
+	@Test
+	public void findNextPositivePowerOfTwoTest2() {          
+		     Assert.assertTrue(MathUtils.findNextPositivePowerOfTwo(-1)==1);
+	}
+	
+	
+}
